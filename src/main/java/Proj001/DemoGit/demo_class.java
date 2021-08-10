591df71906d89517bbfa3ccf62749ebeb52d63b1
aravi <aravi@LAPTOP-OG7K321F.broadband>
2021-08-10 18:59:43 +0100

    First commit

---
+++ src/main/java/Proj001/DemoGit/demo_class.java
@@ -0,0 +1,11 @@
+package Proj001.DemoGit;
+
+public class demo_class {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Test title");
+
+	}
+
+}
